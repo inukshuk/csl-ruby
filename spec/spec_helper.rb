52d3dd53bf9a858c1628f92f1297418e539592a7
Sylvester Keil <sylvester@keil.or.at>
2012-07-14 17:33:19 +0200

    added style/locale fixtures for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,4 +27,15 @@ end
 
 RSpec.configure do |config|
   config.include(SilentWarnings)
+
+  config.before :all do
+    style_root, locale_root = CSL::Style.root, CSL::Locale.root
+
+    CSL::Style.root  = File.expand_path('../fixtures/styles',  __FILE__)
+    CSL::Locale.root = File.expand_path('../fixtures/locales', __FILE__)
+  end
+
+  config.after :all do
+    CSL::Style.root, CSL::Locale.root = style_root, locale_root
+  end
 end
