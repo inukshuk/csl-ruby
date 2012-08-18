2484f2cc6de9a83e91c341a0f74787ce593abb86
Sylvester Keil <sylvester@keil.or.at>
2012-08-18 23:51:07 +0200

    added date accessors for experimental

--- lib/csl/style/date.rb
+++ lib/csl/style/date.rb
@@ -1,16 +1,93 @@
 module CSL
   class Style
-    
+
     class Date < Node
+
+      attr_defaults :'date-parts' => 'year-month-day'
+
       attr_struct :name, :form, :'range-delimiter', :'date-parts', :variable,
-        :'text-case', *Schema.attr(:affixes, :display, :font, :delimiter)   
+        *Schema.attr(:display, :formatting, :delimiter)
+
+      attr_children :'date-part'
+
+      alias date_parts date_part
+      alias parts date_part
+
+      private :date_part
+
+      def initialize(attributes = {})
+        super(attributes, &nil)
+        children[:'date-part'] = []
+
+        yield self if block_given?
+      end
+
+      def delimiter
+        attributes.fetch(:delimiter, '')
+      end
+
+      def has_variable?
+        attribute?(:variable)
+      end
+
+      def variable
+        attributes[:variable]
+      end
+
+      def has_form?
+        attribute?(:form)
+      end
+
+      def form
+        attributes[:form]
+      end
+
+      def has_date_parts?
+        !date_parts.empty?
+      end
+      alias has_parts? has_date_parts?
+
     end
-    
+
     class DatePart < Node
-      attr_struct :name, :form, :'range-delimiter', :'text-case',
-        *Schema.attr(:affixes, :font, :periods)
+      attr_struct :name, :form, :'range-delimiter',
+        *Schema.attr(:formatting, :periods)
+
+      def range_delimiter
+        attributes.fetch(:'range-delimiter', '')
+      end
+
+      def name
+        attributes[:name].to_s
+      end
+
+      def has_form?
+        attribute?(:form)
+      end
+
+      def form
+        case
+        when has_form?
+          attributes[:form]
+        when has_parent?
+          parent.form
+        else
+          nil
+        end
+      end
+
+      def year?
+        name =~ /year/i
+      end
+
+      def month?
+        name =~ /month/i
+      end
+
+      def day?
+        name =~ /day/i
+      end
     end
-    
-    
+
   end
 end
--- lib/csl/style/number.rb
+++ lib/csl/style/number.rb
@@ -15,28 +15,35 @@ module CSL
 				attributes[:variable]
 			end
 
+			def has_form?
+				attribute?(:form)
+			end
+
+			def form
+				attributes[:form]
+			end
+
       # @return [Boolean] whether or not the number's format is set to
       #   :numeric; also returns true if the number's form attribute is not
       #   set or nil.
       def numeric?
-        !attribute?(:form) || attributes.form.to_sym == :numeric
+        !has_form? || form.to_sym == :numeric
       end
 
       # @return [Boolean] whether or not the number's format is set to :ordinal
       def ordinal?
-				attribute?(:form) && attributes.form.to_sym == :ordinal
+				has_form? && form.to_sym == :ordinal
       end
 
       # @return [Boolean] whether or not the number's format is set to :'long-ordinal'
       def long_ordinal?
-        attribute?(:form) && attributes.form.to_sym == :'long-ordinal'
+        has_form? && form.to_sym == :'long-ordinal'
       end
 
       # @return [Boolean] whether or not the number's format is set to :roman
       def roman?
-        attribute?(:form) && attributes.form.to_sym == :roman
+        has_form? && form.to_sym == :roman
       end
-
     end
 
   end
