7e5469f650ea9834b264524b055dbaec0d27b98c
Sylvester Keil <sylvester@keil.or.at>
2013-01-18 21:50:16 +0100

    add label specs

--- spec/csl/style/label_spec.rb
+++ spec/csl/style/label_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 module CSL
   describe Style::Label do
-    let(:label) { Style::Label.new }
+    
+    it { should_not be_names_label }
+    it { should_not be_always_pluralize }
+    it { should_not be_never_pluralize }
+    
+    describe 'a label inside a names node' do
+      before(:each) { Style::Names.new << subject }
+      
+      it { should be_names_label }
+      
+      describe '#variable' do
+        before(:each) { subject.parent[:variable] = 'editor' }
+
+        it 'returns the names variable(s)' do
+          subject.variable.should == 'editor'
+        end
+        
+        it 'returns the names variable(s) even when the local attribute is set' do
+          subject[:variable] = 'page'
+          subject.variable.should == 'editor'
+        end
+      end
+    end
   end
 end
