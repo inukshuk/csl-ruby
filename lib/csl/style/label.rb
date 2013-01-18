faee0bb7717de5cdf96c35c3dbb380b95cc6b9df
Sylvester Keil <sylvester@keil.or.at>
2013-01-18 22:23:46 +0100

    add variable-term mapping to label

--- lib/csl/style/label.rb
+++ lib/csl/style/label.rb
@@ -1,6 +1,14 @@
 module CSL
   class Style
 
+    # Labels are used for printing the term matching the variable selected
+    # with the required variable attribute, which must be set to "locator,
+    # "page", or one of the number variables.
+    #
+    # The term is only rendered if the selected variable is non-empty.
+    #
+    # If the {Label} is the child of a {Names} node, the parent's variable
+    # is used instead of the local attribute.
     class Label < Node
 
       attr_struct :variable, :form, :plural,
@@ -10,8 +18,21 @@ module CSL
 
       @variables = [:locator, :page].concat(Schema.variables[:number]).freeze
 
+      @terms = Hash.new { |h,k| h[k] = k }
+
+      @terms['number-of-pages'] = 'page'
+      @terms['number-of-volumes'] = 'volume'
+      @terms['chapter-number'] = 'chapter'
+      @terms['collection-number'] = 'collection'
+
       class << self
+        # @!attribute [r] variables
+        # @return [Array<Symbol>] a list of valid label variable values
         attr_reader :variables
+
+        # @!attribute [r] terms
+        # @return [Hash<String, String>] a cache for variable-term mapping
+        attr_reader :terms
       end
 
       def has_variable?
@@ -51,6 +72,9 @@ module CSL
       end
       alias name_label? names_label?
 
+      def term
+        Label.terms[variable]
+      end
     end
 
   end
--- spec/csl/style/label_spec.rb
+++ spec/csl/style/label_spec.rb
@@ -2,28 +2,56 @@ require 'spec_helper'
 
 module CSL
   describe Style::Label do
-    
+
     it { should_not be_names_label }
     it { should_not be_always_pluralize }
     it { should_not be_never_pluralize }
-    
+
+    describe '.terms' do
+      Hash[%w{
+        page              page
+        issue             issue
+        edition           edition
+        number-of-pages   page
+        number-of-volumes volume
+        chapter-number    chapter
+      }].each do |variable, term|
+        it "returns #{term.inspect} for #{variable.inspect}" do
+          Label.terms[variable].should == term
+        end
+      end
+    end
+
     describe 'a label inside a names node' do
       before(:each) { Style::Names.new << subject }
-      
+
       it { should be_names_label }
-      
+
       describe '#variable' do
         before(:each) { subject.parent[:variable] = 'editor' }
 
         it 'returns the names variable(s)' do
           subject.variable.should == 'editor'
         end
-        
+
         it 'returns the names variable(s) even when the local attribute is set' do
           subject[:variable] = 'page'
           subject.variable.should == 'editor'
         end
       end
     end
+
+    describe '#term' do
+      it { should be_empty }
+
+      it 'returns the term for the current variable' do
+        subject[:variable] = 'page'
+        subject.term.should == 'page'
+
+        subject[:variable] = 'number-of-volumes'
+        subject.term.should == 'volume'
+      end
+    end
+
   end
 end
