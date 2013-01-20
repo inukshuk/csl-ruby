e1b524242073d517d6066dbc926e74164ecb2a08
Sylvester Keil <sylvester@keil.or.at>
2013-01-20 22:12:37 +0100

    prepare choose blocks for rendering

--- lib/csl/style/choose.rb
+++ lib/csl/style/choose.rb
@@ -1,16 +1,21 @@
 module CSL
   class Style
-    
+
     class Choose < Node
 
       class Block < Node
-        
-        def self.matches?(nodename)
-          nodename.to_s =~ /^if(-else)?|else$/
+        attr_struct :match, :disambiguate, :'is-numeric', :'is-uncertain-date',
+          :locator, :position, :type, :variable
+
+        attr_defaults :match => 'all'
+
+        class << self
+          def matches?(nodename)
+            nodename.to_s =~ /^if(-else)?|else$/
+          end
         end
-        
       end
     end
-    
+
   end
 end
