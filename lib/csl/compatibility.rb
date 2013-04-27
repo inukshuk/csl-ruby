7af69bbf21bb2d330171064bd6a843d077ed4f20
Sylvester Keil <sylvester@keil.or.at>
2013-04-27 15:19:34 +0200

    1.0.0-patch.1

--- lib/csl/compatibility.rb
+++ lib/csl/compatibility.rb
@@ -8,18 +8,7 @@ class Symbol
   end
 
   def match(pattern)
-    str = to_s
-
-    case pattern
-    when Regexp
-      match_data = pattern.search_region(str, 0, str.bytesize, true)
-      Regexp.last_match = match_data
-      return match_data.full[0] if match_data
-    when String
-      raise TypeError, "type mismatch: String given"
-    else
-      pattern =~ str
-    end
+    to_s.match(pattern)
   end
 
   alias =~ match
--- lib/csl/version.rb
+++ lib/csl/version.rb
@@ -1,3 +1,3 @@
 module CSL  
-  VERSION = '1.0.0'.freeze
+  VERSION = '1.0.0-patch.1'.freeze
 end
