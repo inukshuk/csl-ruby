0ae8bf9311485440fcb84c235951061a801326c9
Sylvester Keil <sylvester@keil.or.at>
2014-01-08 12:19:40 +0100

    whitespace

--- lib/csl/version.rb
+++ lib/csl/version.rb
@@ -1,3 +1,3 @@
-module CSL  
+module CSL
   VERSION = '1.0.2'.freeze
 end
--- spec/csl/schema_spec.rb
+++ spec/csl/schema_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 module CSL
   describe 'Schema' do
-  
+
     it 'cannot be instantiated' do
       Schema.should_not respond_to(:new)
     end
-    
+
     describe '.version' do
       it 'returns a version string' do
         Schema.version.should match(/^\d+\.\d+\.\d+/)
       end
-      
+
       it 'is greater than 1.0' do
         Schema.version.split(/\./)[0].to_i.should >= 1
       end
     end
-    
+
     describe '.variables' do
       it 'contains :names fields' do
         Schema.variables[:names].should_not be_empty
@@ -36,43 +36,43 @@ module CSL
         Schema.variables[:numbers].should_not be_empty
         Schema.variables[:number].should_not be_empty
       end
-      
+
       it 'accepts either string or symbol input' do
         Schema.variables[:names].should equal Schema.variables['names']
-      end  
+      end
     end
-    
+
     describe '.types' do
       it 'returns an array' do
         Schema.types.should be_a(Array)
       end
-      
+
       it 'is not empty' do
         Schema.types.should_not be_empty
       end
-      
+
       it 'includes :article' do
         Schema.types.should include(:article)
       end
     end
-    
+
     describe '.categories' do
       it 'given a field name returns the corresponding type' do
         Schema.categories.values_at(:author, :issued, :abstract, :issue).should ==
           [:names, :date, :text, :number]
       end
-      
+
       it 'accepts either string or symbol input' do
         Schema.categories.should have_key(:author)
         Schema.categories['author'].should equal Schema.categories[:author]
       end
     end
-    
+
     describe '.validate' do
       it 'accepts and validates a locale instance' do
         Schema.validate(Locale.load('en-US')).should == []
       end
-      
+
       it 'accepts and validates a locale file path' do
         Schema.validate(File.join(Locale.root, 'locales-en-US.xml')).should == []
       end
@@ -103,9 +103,9 @@ module CSL
          </stle>
         })[0][0].should == 0 # error on line 0 -> parse error
       end
-      
+
     # TODO fix nokogiri/jing validation
     end unless RUBY_PLATFORM =~ /java/i
-    
+
   end
 end
